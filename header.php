2930d31bc72bc54ffbf4a50841ef20de9a403ac7
Sam Allemang <sca@scallemang.ca>
2015-02-08 00:31:40 -0500

    Menu items are called in header.php

--- header.php
+++ header.php
@@ -32,15 +32,16 @@
           <a class="navbar-brand" href="<?php bloginfo( 'url' ); ?>"><?php bloginfo( 'name' ); ?></a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
-          <form class="navbar-form navbar-right">
-            <div class="form-group">
-              <input type="text" placeholder="Email" class="form-control">
-            </div>
-            <div class="form-group">
-              <input type="password" placeholder="Password" class="form-control">
-            </div>
-            <button type="submit" class="btn btn-success">Sign in</button>
-          </form>
+          
+          <?php
+            $args = array(
+              'menu'        =>  'header-menu',
+              'menu_class'  =>  'nav navbar-nav',
+              'container'   =>  'false'
+            );
+            wp_nav_menu( $args );
+          ?>
+
         </div><!--/.navbar-collapse -->
       </div>
     </nav>
