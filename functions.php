aceda55cca211a79423e8b20dd181c9a549c83a8
Sam Allemang <sca@scallemang.ca>
2015-02-08 00:22:26 -0500

    Added support for menus

--- functions.php
+++ functions.php
@@ -25,4 +25,15 @@ function theme_js() {
 
 add_action( 'wp_enqueue_scripts', 'theme_js' );
 
-?>
+add_theme_support( 'menus' );
+
+function register_theme_menus() {
+
+	register_nav_menus(
+			array(
+				'header-menu'	=>	__( 'Header Menu' )
+			)
+	);
+}
+
+add_action( 'init', 'register_theme_menus' );
