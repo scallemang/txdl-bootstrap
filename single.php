e7585393af10705a72cfb980cf07ebd2c49b21c4
Sam Allemang <sca@scallemang.ca>
2015-02-09 15:16:21 -0500

    Added comments template and metadata to single.php.

--- single.php
+++ single.php
@@ -7,8 +7,11 @@
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
           <div class="page-header">
             <h1><?php the_title(); ?></h1>
+            <p><em>By <?php the_author(); ?> on <?php echo the_time('l, F jS, Y'); ?> in <?php the_category(', '); ?>. <a href="<?php comments_link(); ?>"><?php comments_number(); ?></a></em></p>
           </div>
           <?php the_content(); ?>
+          <hr>
+          <?php comments_template(); ?>
         <?php endwhile; else: ?>
           <div class="page-header">
             <h1>There isn&rsquo;t any content here!</h1>
