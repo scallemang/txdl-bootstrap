c13342b1b919db6dedba3c1f981d428ff0ddce9a
Sam Allemang <sca@scallemang.ca>
2015-02-09 11:09:32 -0500

    Added blog sidebar.

--- home.php
+++ home.php
@@ -21,6 +21,6 @@
           </div>
         <?php endif; ?>
      </div>
-      <?php get_sidebar(); ?>
+      <?php get_sidebar( 'blog' ); ?>
     </div>
 <?php get_footer(); ?>
---
+++ sidebar-blog.php
@@ -0,0 +1,6 @@
+<div class="col-md-3 sidebar">
+	<?php if ( ! dynamic_sidebar( 'blog' ) ): ?>
+		<h2>Sidebar Setup</h2>
+		<p>Please add widgets to the blog sidebar to have them display here.</p>
+	<?php endif; ?>
+</div>
