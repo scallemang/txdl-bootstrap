8b8338f8cb79e1ff3cb19751c209cf6fc1e2f207
Sam Allemang <sca@scallemang.ca>
2015-02-09 15:13:45 -0500

    Added single.php; minor styling to home.php.

--- home.php
+++ home.php
@@ -11,8 +11,9 @@
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
           <article>
             <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-            <p>By <?php the_author(); ?> on <?php echo the_time('l, F jS, Y'); ?> in <?php the_category(', '); ?>. <a href="<?php comments_link(); ?>"><?php comments_number(); ?></a></p>
+            <p><em>By <?php the_author(); ?> on <?php echo the_time('l, F jS, Y'); ?> in <?php the_category(', '); ?>. <a href="<?php comments_link(); ?>"><?php comments_number(); ?></a></em></p>
             <?php the_excerpt(); ?>
+            <hr>
           </article>
         <?php endwhile; else: ?>
           <div class="page-header">
