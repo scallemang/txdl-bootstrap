e75ef05f464562d90505fab362b83127ad85cf0e
Sam Allemang <sca@scallemang.ca>
2015-02-08 23:42:38 -0500

    Created full width page template.

---
+++ page-full-width.php
@@ -0,0 +1,24 @@
+<?php
+/*
+Template Name: Full Width
+*/
+<?php get_header(); ?>
+
+  <div class="container">
+    <div class="row">
+      
+      <div class="col-md-12">
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+          <div class="page-header">
+            <h1><?php the_title(); ?></h1>
+          </div>
+          <?php the_content(); ?>
+        <?php endwhile; else: ?>
+          <div class="page-header">
+            <h1>There isn&rsquo;t any content here!</h1>
+            <p>Someone must have misplaced this amazing page&hellip;.</p>
+          </div>
+        <?php endif; ?>
+     </div>
+    </div>
+<?php get_footer(); ?>
