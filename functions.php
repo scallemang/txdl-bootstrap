e49c2d8a5189b344bd407c4f91f0438e947c37fc
Sam Allemang <sca@scallemang.ca>
2015-02-08 23:26:54 -0500

    Registered sidebar widget.

--- functions.php
+++ functions.php
@@ -55,5 +55,6 @@ function create_widget( $name, $id, $description ) {
 create_widget( 'Front Page Left', 'front-left', 'Displays on the left of the homepage' );
 create_widget( 'Front Page Center', 'front-center', 'Displays in the center of the homepage' );
 create_widget( 'Front Page Right', 'front-right', 'Displays on the right of the homepage' );
+create_widget( 'Page Sidebar', 'page', 'Displays on the side of pages with a sidebar' );
 
 ?>
