3dceac4ecb432caab29d28ae90cf4056d6e4ca99
Sam Allemang <sca@scallemang.ca>
2015-02-09 15:37:23 -0500

    Added archive.php.

---
+++ archive.php
@@ -0,0 +1,27 @@
+<?php get_header(); ?>
+
+  <div class="container">
+    <div class="row">
+      
+      <div class="col-md-9">
+        <div class="page-header">
+          <h1><?php wp_title(''); ?></h1>
+        </div>
+
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+          <article>
+            <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+            <p><em>By <?php the_author(); ?> on <?php echo the_time('l, F jS, Y'); ?> in <?php the_category(', '); ?>. <a href="<?php comments_link(); ?>"><?php comments_number(); ?></a></em></p>
+            <?php the_excerpt(); ?>
+            <hr>
+          </article>
+        <?php endwhile; else: ?>
+          <div class="page-header">
+            <h1>There isn&rsquo;t any content here!</h1>
+            <p>Someone must have misplaced this amazing page&hellip;.</p>
+          </div>
+        <?php endif; ?>
+     </div>
+      <?php get_sidebar( 'blog' ); ?>
+    </div>
+<?php get_footer(); ?>
