36c6ddbfad4f1a1ad0f8f0dca2715b6b2fa99beb
Sam Allemang <sca@scallemang.ca>
2015-02-08 23:19:02 -0500

    Call for sidebar in page.php, created sidebar.php file.

--- page.php
+++ page.php
@@ -16,10 +16,6 @@
           </div>
         <?php endif; ?>
      </div>
-      <div class="col-md-3">
-        <h2>Heading</h2>
-        <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-        <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-      </div>
+      <?php get_sidebar(); ?>
     </div>
 <?php get_footer(); ?>
---
+++ sidebar.php
@@ -0,0 +1,5 @@
+<div class="col-md-3">
+	<h2>Sidebar</h2>
+	<p>Donec id elit non mi porta gravida et eget metus.</p>
+	<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+</div>
