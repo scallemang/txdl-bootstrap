aef99ffd98b654481b464b4bf31e7a2beb534db7
Sam Allemang <sca@scallemang.ca>
2015-02-08 23:15:03 -0500

    Page title and content are dynamic.

--- page.php
+++ page.php
@@ -4,9 +4,17 @@
     <div class="row">
       
       <div class="col-md-9">
-        <h2>Heading</h2>
-        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-        <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+          <div class="page-header">
+            <h1><?php the_title(); ?></h1>
+          </div>
+          <?php the_content(); ?>
+        <?php endwhile; else: ?>
+          <div class="page-header">
+            <h1>There isn&rsquo;t any content here!</h1>
+            <p>Someone must have misplaced this amazing page&hellip;.</p>
+          </div>
+        <?php endif; ?>
      </div>
       <div class="col-md-3">
         <h2>Heading</h2>
