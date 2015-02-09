141633d16ed854c4ceeb414271efccdb70b75bbf
Sam Allemang <sca@scallemang.ca>
2015-02-08 21:38:34 -0500

    Added favicon link and placeholder .ico.

--- header.php
+++ header.php
@@ -4,9 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="../../favicon.ico">
+    <link rel="icon" href="<?php bloginfo('template_directory');?>/images/favicon.ico">
 
     <title>Jumbotron Template for Bootstrap</title>
 
