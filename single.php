567d10b16da89fe7a4e6a851643a94dd5db37607
Sam Allemang <sca@scallemang.ca>
2015-02-13 07:14:39 -0500

    Added featured images to single posts.

--- single.php
+++ single.php
@@ -7,6 +7,12 @@
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
           <div class="page-header">
             <h1><?php the_title(); ?></h1>
+            <?php
+              $thumbnail_id = get_post_thumbnail_id(); 
+              $thumbnail_url = wp_get_attachment_image_src( $thumbnail_id, 'thumbnail-size', true );
+              $thumbnail_meta = get_post_meta( $thumbnail_id, '_wp_attachment_image_alt', true);                
+            ?>
+            <p><img src="<?php echo $thumbnail_url[0]; ?>" alt="<?php echo $thumbnail_meta; ?>" class="img-responsive"></p>
             <p><em>By <?php the_author(); ?> on <?php echo the_time('l, F jS, Y'); ?> in <?php the_category(', '); ?>. <a href="<?php comments_link(); ?>"><?php comments_number(); ?></a></em></p>
           </div>
           <?php the_content(); ?>
