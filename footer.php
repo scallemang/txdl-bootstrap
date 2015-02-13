c99f7019c0c378dc4cb1612d26cbf292bd663f81
Sam Allemang <sca@scallemang.ca>
2015-02-13 09:03:40 -0500

    Added Ninja Forms snippet and modal window code to footer.php.

--- footer.php
+++ footer.php
@@ -5,6 +5,26 @@
       </footer>
     </div> <!-- /container -->
 
+    <div class="modal fade">
+	  <div class="modal-dialog">
+	    <div class="modal-content">
+	      <div class="modal-header">
+	        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+	        <h4 class="modal-title">Contact Us</h4>
+	      </div>
+	      <div class="modal-body">
+	        <?php
+	        	if( function_exists( 'ninja_forms_display_form' ) ){ ninja_forms_display_form( 1 ); }
+	        ?>
+	      </div>
+	      <div class="modal-footer">
+	        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+	        <button type="button" class="btn btn-primary">Save changes</button>
+	      </div>
+	    </div><!-- /.modal-content -->
+	  </div><!-- /.modal-dialog -->
+	</div><!-- /.modal -->
+
     <?php wp_footer(); ?>
   </body>
 </html>
