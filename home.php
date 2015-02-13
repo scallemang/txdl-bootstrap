f29c669a61ce28220d716579c099bf056666fb58
Sam Allemang <sca@scallemang.ca>
2015-02-13 05:34:21 -0500

    Began adding carousel code, including tests.

--- home.php
+++ home.php
@@ -8,6 +8,56 @@
           <h1><?php wp_title(''); ?></h1>
         </div>
 
+        <?php
+
+          $args = array(
+              'post_type' =>  'post',
+              'category_name' =>  'featured'
+            );
+
+          $the_query = new WP_Query( $args );
+
+        ?>
+
+        <?php if ( have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+          <?php the_title(); ?></p>
+        <?php endwhile; endif; ?>
+
+        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+          <!-- Indicators -->
+          <ol class="carousel-indicators">
+            <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+            
+          </ol>
+
+          <!-- Wrapper for slides -->
+          <div class="carousel-inner" role="listbox">
+            <div class="item active">
+              <img src="..." alt="...">
+              <div class="carousel-caption">
+                ...
+              </div>
+            </div>
+            <div class="item">
+              <img src="..." alt="...">
+              <div class="carousel-caption">
+                ...
+              </div>
+            </div>
+            ...
+          </div>
+
+          <!-- Controls -->
+          <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
+
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
           <article>
             <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
