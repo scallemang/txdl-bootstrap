2fbbbb33bba3020d2d4f409aa2f44230b9f5322b
Sam Allemang <sca@scallemang.ca>
2015-02-13 09:40:07 -0500

    Added toggle HTML to page.php.

--- page.php
+++ page.php
@@ -1,9 +1,12 @@
 <?php get_header(); ?>
 
   <div class="container">
-    <div class="row">
+    <div class="row row-offcanvas row-offcanvas-right">
       
       <div class="col-md-9">
+        <p class="pull-right visible-xs">
+          <button type="button" class="btn btn-primary btn-xs" data-toggle="offcanvas">Toggle sidebar</button>
+        </p>
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
           <div class="page-header">
             <h1><?php the_title(); ?></h1>
