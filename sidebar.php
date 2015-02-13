051d8e6e8233df4f9aa790c933598e2caddcab29
Sam Allemang <sca@scallemang.ca>
2015-02-13 09:41:04 -0500

    Added offcanvas class to sidebar.php.

--- sidebar.php
+++ sidebar.php
@@ -1,4 +1,4 @@
-<div class="col-md-3 sidebar">
+<div class="col-md-3 sidebar sidebar-offcanvas">
 	<?php if ( ! dynamic_sidebar( 'page' ) ): ?>
 		<h2>Sidebar Setup</h2>
 		<p>Please add widgets to the page sidebar to have them display here.</p>
